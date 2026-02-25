7c848525f2a5ec5171e06997f6cd9c3b2fa5a2fc
hick-hpe <hpalermoemerick@gmail.com>
2026-02-25 08:12:49 -0300

    feat: ajustar layout do formulário de login

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Categoria(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
